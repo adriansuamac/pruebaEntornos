152b79985ea1490c68de2359ea5b525cdf2a24e2
adrian <adrian@programa.com>
2025-01-10 09:56:03 +0100

    He modificado los archivos app.java y readme.md

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
-  public static void main(String[] args) {
-    System.out.println("Contenido 2a");
-    System.out.println("Contenido 3");
-    System.out.println("Contenido 2b");
+  public static void main(String[] args) { 
+
+	System.out.println("Contenido 2a"); 
+	System.out.println("Contenido 3"); 
+	System.out.println("Contenido 2b");    	
+	System.out.println("Adrian Suarez"); 
+	System.out.println("IES San Andrés");
   }
   
 }
